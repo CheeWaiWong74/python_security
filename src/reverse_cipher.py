594b20c856fbb478923b101b5975c657a6806944
cheewai wong <ytransform_academics@outlook.com>
2023-10-30 01:44:17 +0800

    Commit reverse_cipher.py

---
+++ src/reverse_cipher.py
@@ -0,0 +1,11 @@
+message = "This program explain reverse cipher."
+translated = ""
+
+i = len(message) - 1
+
+while i >= 0:
+    translated = translated + message[i]
+    i = i - 1
+
+print("The cipher text is : ", translated)
+    
